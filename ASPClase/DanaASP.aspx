0b6816ecb5b50dbaa84cee020e0f185d981cc018
airplanedana <dana.gomez@airplane.solutions>
2022-12-15 13:30:15 +0100

    Agregar archivos de proyecto.

---
+++ ASPClase/DanaASP.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DanaASP.aspx.cs" Inherits="ASPClase.DanaASP" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>  
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click1" />
+
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+
+        <h2>Selecciona fecha del calendario</h2>  
+        <div>  
+            <asp:Calendar ID="Calendar1" runat="server"   
+            OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>  
+        </div>  
+    </form>  
+    <p>  
+        <asp:Label runat="server" ID="ShowDate" ></asp:Label>  
+    </p>  
+</body>
+</html>
---
+++ ASPClase/DanaASP.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ASPClase
+{
+    public partial class DanaASP : System.Web.UI.Page
+    {
+        Dictionary<string, DateTime> birthdays = new Dictionary<string, DateTime>();
+        string connectionStr = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=cumples;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Hacemos connexion BBDD
+            SqlConnection con = new SqlConnection(connectionStr);
+            string query = "SELECT * FROM persona";
+            SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+
+            // Añadimos los cumpleaños al diccionario
+            birthdays.Add("Dana", new DateTime(1900, 1, 16));
+            birthdays.Add("Marc", new DateTime(1900, 5, 30));
+            birthdays.Add("Jordi", new DateTime(1900, 12, 22));
+            birthdays.Add("Sergi", new DateTime(1900, 8, 12));
+            birthdays.Add("Michael", new DateTime(1900, 12, 10));
+            birthdays.Add("Daniel", new DateTime(1900, 7, 1));
+
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataBind();
+            DropDownList1.DataTextField = "nombre";
+            DropDownList1.DataValueField = "id";
+            DropDownList1.DataBind();
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionStr);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM persona where id = '" + DropDownList1.SelectedValue + "'", con);
+            SqlDataAdapter Adpt = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            Adpt.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+            Label1.Text = DropDownList1.SelectedValue;
+        }
+
+        public void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            ShowDate.Text = "Has seleccionado: " + Calendar1.SelectedDate.ToString("D");
+
+            foreach (var person in birthdays)
+            {
+                if (person.Value.Month == Calendar1.SelectedDate.Month && person.Value.Day == Calendar1.SelectedDate.Day)
+                {
+                    ShowDate.Text += "<br> Es el cumple de " + person.Key;
+                }
+            }
+        }
+    }
+}
